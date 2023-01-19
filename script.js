aa57a13fa53ddcdff764e98cb0c2c9aa721951b0
JessicaCavassola <114621485+JessicaCavassola@users.noreply.github.com>
2023-01-19 16:49:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,32 +9,29 @@ function contar (){
 
 if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
     res.innerHTML = ("Impossivel contar")
-    
-    
-
+       
     
 } else {
     res.innerHTML = "contando: <br> "
-     /*convertendo inicio fim e passo em Number*/ 
+    
     let i = Number (inicio.value)
     let f = Number (fim.value)
     let p = Number (passo.value)
-    /*se o passo  for menor ou = a zero*/
+   
     if  (p <= 0){
         window.alert("Passo invalido. leiderando Passo 1")
         p = 1
     }
     if (i < f){
         /*CONTAGEM CRESCENTE*/
-        /*declarando c para armezenar o resultado antes de atribuir este valor a res*/
-        for (let c = i; c <= f; c += p) { /*contador vai começar no INICIO e enquanto contador for <= a FIM o contador recebe ele mais o passo */
+        
+        for (let c = i; c <= f; c += p) { 
         res.innerHTML += ` ${c} \u{1F449}`
     }   
     } else {
         /*CONTAGEM DECRESCENTE*/
         for (let c = i; c >= f; c -= p){
             res.innerHTML += ` ${c} \u{1F449}`
-
     }
     }    
     res.innerHTML += `\u{2B55}`
